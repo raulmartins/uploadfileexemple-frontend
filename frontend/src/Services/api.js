b75263028f6f78df27e6e812033ddbf766c925d7
Raul Lima <raul.lima@globalhitss.com.br>
2019-03-24 22:41:37 -0300

    .gitignore

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  //baseURL: 'http://localhost:3000'
-  baseURL: 'https://uploadfileexemple-backend.herokuapp.com'
+  baseURL: process.env.REACT_APP_API_URL
 })
 
 export default api;
